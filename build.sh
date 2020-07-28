1ff817d73960a165c77d76cc1d2fe135583fa29a
Mikko Kotila <mailme@mikkokotila.com>
2020-07-28 21:09:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -47,9 +47,9 @@ sudo docker login docker.pkg.github.com --username mikkokotila --password $MIKKO
 # run Padma-API
 sudo docker pull docker.pkg.github.com/mikkokotila/padma/core_api:master
 NEW_IMAGE_ID=$(sudo docker images | grep core_api | staging | tail -1 | tr -s ' ' | cut -d ' ' -f3)
-sudo docker run --restart unless-stopped -v /home/ubuntu/Padma-Data:/tmp -p 5000:5000 --detach $NEW_IMAGE_ID --name Padma-API;
+sudo docker run --restart unless-stopped -v /home/ubuntu/Padma-Data:/tmp --name Padma-API -p 5000:5000 --detach $NEW_IMAGE_ID;
 
 # run Padma-Frontend
 sudo docker pull docker.pkg.github.com/mikkokotila/padma-frontend/frontend:master
 NEW_IMAGE_ID=$(sudo docker images | grep frontend | staging | tail -1 | tr -s ' ' | cut -d ' ' -f3)
-sudo docker run --restart unless-stopped -v /home/ubuntu/Padma-Data:/tmp -p 8080:8080 --detach $NEW_IMAGE_ID --name Padma-Frontend;
+sudo docker run --restart unless-stopped -p 8080:8080 --detach $NEW_IMAGE_ID;
