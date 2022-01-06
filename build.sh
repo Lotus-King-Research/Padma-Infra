3eb034cfe818818220b745c5a503facc68aa3878
Mikko Kotila <mailme@mikkokotila.com>
2022-01-06 16:09:58 +0200

    added memory flags

--- build.sh
+++ build.sh
@@ -38,11 +38,11 @@ sudo docker login docker.pkg.github.com --username mikkokotila --password $MIKKO
 #sudo docker pull docker.pkg.github.com/lotus-king-trust/padma-backend/core_api:master
 sudo docker pull ghcr.io/lotus-king-research/padma-backend/core_api:master
 NEW_IMAGE_ID=$(sudo docker images | grep core_api | grep master | tail -1 | tr -s ' ' | cut -d ' ' -f3)
-sudo docker run --restart unless-stopped --name Padma-API -p 5000:5000 --detach $NEW_IMAGE_ID;
+sudo docker --memory 200m --memory-swap 200m run --restart unless-stopped --name Padma-API -p 5000:5000 --detach $NEW_IMAGE_ID;
 
 # run Padma-Frontend
 # sudo docker pull docker.pkg.github.com/lotus-king-trust/padma-frontend/frontend:master
 # sudo docker pull ghcr.io/lotus-king-research/padma-frontend/frontend:master
 sudo docker pull ghcr.io/lotus-king-research/padma-frontend/frontend:master
 NEW_IMAGE_ID=$(sudo docker images | grep frontend | grep master | tail -1 | tr -s ' ' | cut -d ' ' -f3)
-sudo docker run --restart unless-stopped --name Padma-Frontend -p 8080:8080 --detach $NEW_IMAGE_ID;
+sudo docker run --memory 500m --memory-swap 500m --restart unless-stopped --name Padma-Frontend -p 8080:8080 --detach $NEW_IMAGE_ID;
